83d0362273263ad7b395a3a96b52ef553a4d91e6
mmestiyak <mmestiyak@gmail.com>
2022-02-11 21:44:24 +0600

    database ur

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use('/', revewsRoutes)
 
 // connect to db
 mongoose.connect(`${process.env.DB}`, (r) => console.log('connected to DB', r))
+console.log("Database_URL", process.env.DATABASE_URL);
 
 // listen to server
 app.listen(process.env.PORT || 3000, () => console.log('listening to port'))
